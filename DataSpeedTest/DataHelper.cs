0a971c633a6108eab6d9064349cc2947eb718c4e
Lukas <yl1882@nyu.edu>
2017-04-25 21:03:47 -0400

    loaded data from sql server by ef

---
+++ DataSpeedTest/DataHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataSpeedTest.Data.EF;
+
+namespace DataSpeedTest
+{
+    class DataHelper
+    {
+        public static List<SourceTable1> LoadDataToList()
+        {
+            using (var context = new TestEntities())
+            {
+                // Query for all blogs with names starting with B 
+                var records = (from r in context.SourceTable1 select r);// context.SourceTable1.Select(r =>r.col1 != null);   // from b in context.TargetTable1 select b;
+                var dd = records.AsEnumerable<SourceTable1>().ToList();
+             
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DataSpeedTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataSpeedTest.Data.EF;
+ 
+
+namespace DataSpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var records = DataHelper.LoadDataToList();
+ 
+
+        }
+    }
+}
